cf96e398678d95bcc7d48c932cae5ec0f10188de
pawan.kumar10 <pawancse@hotmail.com>
2019-12-23 22:44:32 +0530

    changes

--- currentApi.js
+++ currentApi.js
@@ -14,7 +14,7 @@ module.exports.nationalNews = function () {
         console.log(response);
         var wordpress = require("wordpress");
         var client = wordpress.createClient({
-            url: "https://proxap.in/",
+            url: "http://proxap.in/",
             username: "admin",
             password: "a2XjCa$X$3"
         });
