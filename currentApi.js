65ad47e14bef8e34f1e944de4cbc566710ffc2f2
pawan.kumar10 <pawancse@hotmail.com>
2019-12-31 09:52:02 +0530

    changes

--- currentApi.js
+++ currentApi.js
@@ -1,7 +1,7 @@
 module.exports.nationalNews = function () {
     const CurrentsAPI = require('currentsapi');
     const currentsapi = new CurrentsAPI('sSLjPypWDAV3JI0FGYJzJCfG1y-6iIafZNLZ5TaLZzU9XTBb');
-    var hours = new Date().getHours() - 1;
+    var hours = new Date().getHours() - 4;
     var month = new Date().getUTCMonth() + 1;
     var from = new Date().getUTCFullYear() + '-' + month + '-' + new Date().getUTCDate() + 'T' + hours + ':' + new Date().getUTCMinutes() + ':' + new Date().getUTCSeconds();
     return currentsapi.search({
